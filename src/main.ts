5d309465509069d3beab5cc7e4cc4f1560716821
Vincent <vincecreton@hotmail.fr>
2022-04-24 22:09:00 +0200

    Test differents ways to display "2D" elements in scene

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './style.css'
 import * as THREE from "three"
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader"
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import {Object3D} from "three/src/core/Object3D";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
 const canvas = document.querySelector<HTMLDivElement>('canvas#webgl')!
 
@@ -21,12 +22,105 @@ const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
 /**
- * Objects
+ * Built-in arrows
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-// scene.add(mesh)
+
+const dir = new THREE.Vector3( -1, 4, 1 );
+
+//normalize the direction vector (convert to vector of length 1)
+dir.normalize();
+
+const origin = new THREE.Vector3( 0, 0, 0 );
+const length = 3;
+const hex = 0xffff00;
+
+const arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
+scene.add( arrowHelper );
+
+/**
+ * Built-in Line
+ */
+
+const lineMaterial = new THREE.LineBasicMaterial({
+    color: 0x000000
+});
+
+const points = [];
+points.push( new THREE.Vector3( 0, 0, 0 ) );
+points.push( new THREE.Vector3( 1, 1, -2 ) );
+points.push( new THREE.Vector3( 3, 2, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( geometry, lineMaterial );
+scene.add( line );
+
+const circleGeometry = new THREE.CircleGeometry( 0.2,  30);
+const circleMaterial = new THREE.MeshBasicMaterial( { color: 'blue', transparent: true } );
+const circle = new THREE.Mesh( circleGeometry, circleMaterial );
+circle.position.setX(1)
+circle.position.setY(1)
+scene.add( circle );
+
+/**
+ * Shape geometry
+ */
+
+const square = new THREE.Shape();
+square.moveTo(0.2, 0.2);
+square.lineTo(0.2, -0.2);
+square.lineTo(-0.2, -0.2);
+square.lineTo(-0.2, 0.2);
+const squareGeometry = new THREE.ShapeGeometry(square);
+const squareMaterial = new THREE.MeshBasicMaterial({
+    color: 'purple',
+    side: THREE.DoubleSide,
+    depthWrite: false
+});
+const squareMesh = new THREE.Mesh(squareGeometry, squareMaterial);
+scene.add(squareMesh);
+
+const diamond = new THREE.Shape();
+diamond.moveTo(0, 1.2);
+diamond.lineTo(0.2, 1);
+diamond.lineTo(0, 0.8);
+diamond.lineTo(-0.2, 1);
+const diamondGeometry = new THREE.ShapeGeometry(diamond);
+const diamondMaterial = new THREE.MeshBasicMaterial({
+    color: 'red',
+    side: THREE.DoubleSide,
+    depthWrite: false
+});
+const diamondMesh = new THREE.Mesh(diamondGeometry, diamondMaterial);
+scene.add(diamondMesh);
+
+/**
+ * Text
+ */
+
+const fontLoader = new FontLoader();
+
+fontLoader.load( 'assets/fonts/test_font.json', function ( font ) {
+
+    const textGeometry = new TextGeometry( 'Hello three.js!', {
+        font: font,
+        size: 80,
+        height: 5,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 10,
+        bevelSize: 8,
+        bevelOffset: 0,
+        bevelSegments: 5
+    } );
+    const textMaterial = new THREE.MeshBasicMaterial({
+        color: 'black',
+    });
+    const textMesh = new THREE.Mesh(textGeometry, textMaterial)
+    textMesh.scale.set(0.001, 0.001, 0.001)
+    textMesh.position.setX(0.3)
+    scene.add(textMesh)
+} );
 
 /**
  * Sizes
@@ -71,7 +165,7 @@ loader.load(
         object.scale.set(0.01, 0.01, 0.01)
 
         object.position.set(0, 0, 0)
-        scene.add(object)
+        // scene.add(object)
         renderer.render(scene, camera)
     },
     (xhr) => {
@@ -94,7 +188,7 @@ renderer.setSize(sizes.width, sizes.height)
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
-document.querySelector<HTMLDivElement>('.btn-default')?.addEventListener('click', (e) => {
+document.querySelector<HTMLDivElement>('.btn-default')?.addEventListener('click', () => {
     activeAction.play()
 })
 
