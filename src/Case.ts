dc8aee62c8c234f9ca402626a21305b5c24c65ce
Vincent <vincecreton@hotmail.fr>
2022-05-05 11:58:04 +0200

    First animation and interaction for case opening (case V6)

---
+++ src/Case.ts
@@ -0,0 +1,225 @@
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader"
+import * as THREE from "three"
+import * as TWEEN from "@tweenjs/tween.js";
+
+export class Case {
+
+    private mixer: THREE.AnimationMixer
+
+    public object: THREE.Group
+
+    private clock: THREE.Clock
+
+    private loader: FBXLoader;
+    private activeAction: THREE.AnimationAction;
+    private lastAction: THREE.AnimationAction;
+    private animationActions: THREE.AnimationAction[];
+    private modelReady: boolean;
+
+    private mouseDown: boolean
+    private pointer = THREE.Vector2
+    private raycaster = THREE.Raycaster
+    private targets: {[name: string]: THREE.Object3D} = {}
+    private x0: number
+    private blockLoop: boolean
+    private runLastAnim: boolean
+
+    private modelFileName = 'case_flo_v-6.fbx';
+
+    constructor() {
+        this.loader = new FBXLoader()
+        this.animationActions = []
+        this.modelReady = false
+        this.raycaster = new THREE.Raycaster();
+        this.pointer = new THREE.Vector2(-1000000, -1000000);
+        this.mouseDown = false
+        this.blockLoop = false
+        this.clock = new THREE.Clock()
+        this.runLastAnim = false
+    }
+
+    init(callback: Function, camera, controls) {
+        this.loader.load(
+           `assets/models/case/${this.modelFileName}`,
+            (object: THREE.Group) => {
+                this.object = object
+                this.mixer = new THREE.AnimationMixer(this.object)
+                for(let i = 0; i < (this.object as THREE.Object3D).animations.length; i++) {
+                    const animationAction = this.mixer.clipAction(
+                        (this.object as THREE.Object3D).animations[i]
+                    )
+                    this.animationActions.push(animationAction)
+                }
+                this.activeAction = this.animationActions[0]
+                this.modelReady = true
+
+                const tempArray: {[name: string]: THREE.Object3D} = {}
+                object.traverse( (child) => {
+                    if ((child as THREE.Mesh).isMesh) {
+                        tempArray[child.name] = child
+                    }
+                })
+                this.targets = tempArray
+
+                this.activeAction.play()
+                console.log(this.animationActions)
+                console.log(this.targets)
+
+                object.scale.set(0.01, 0.01, -0.01)
+
+                object.position.set(0, 0, 0)
+                callback()
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+            },
+            (error) => {
+                console.log(error)
+            }
+        )
+
+        window.addEventListener( 'pointermove', (e) => {
+            // calculate pointer position in normalized device coordinates
+            // (-1 to +1) for both components
+            const width = document.querySelector('#webgl')?.clientWidth
+            const heigth = document.querySelector('#webgl')?.clientHeight
+            if(width && heigth) {
+                this.pointer.x = ( e.clientX / width ) * 2 - 1;
+                this.pointer.y = - ( e.clientY / heigth ) * 2 + 1;
+            }
+        });
+
+        window.addEventListener('mousedown', () => {
+            if(this.modelReady && !this.blockLoop) {
+                this.raycaster.setFromCamera( this.pointer, camera );
+                const intersects = this.raycaster.intersectObjects(Object.values(this.targets));
+                for ( let i = 0; i < intersects.length; i ++ ) {
+                    if(intersects[i].object.name === "packaging") {
+                        const targetCoords = {
+                            x: 0.0021811573810216803,
+                            y: 0.30347417279793715,
+                            z: 1.3563360735759848
+                        }
+                        const coords = { x: camera.position.x, y: camera.position.y, z: camera.position.z };
+                        new TWEEN.Tween(coords)
+                            .to({ x: targetCoords.x, y: targetCoords.y, z: targetCoords.z })
+                            .onUpdate(() =>
+                                camera.position.set(coords.x, coords.y, coords.z)
+                            )
+                            .start();
+                        this.setAction(this.animationActions[1])
+                        this.activeAction.setLoop(THREE.LoopOnce)
+                        this.activeAction.clampWhenFinished = true
+                        controls.enabled = false
+                        this.triggerSecondAnimation(camera)
+                    }
+                }
+            }
+        })
+    }
+
+    triggerSecondAnimation(camera) {
+        document.querySelector<HTMLCanvasElement>('#webgl')?.addEventListener(
+            'mousedown',
+            (e) => {
+                e.preventDefault()
+                this.mouseDown = true
+            }
+        )
+        document.querySelector<HTMLCanvasElement>('#webgl')?.addEventListener(
+            'mouseup',
+            (e) => {
+                e.preventDefault()
+                this.mouseDown = false
+            }
+        )
+        document.querySelector<HTMLCanvasElement>('#webgl')?.addEventListener(
+            'mousemove',
+            (event) => {
+                if(!this.blockLoop) {
+                    this.raycaster.setFromCamera( this.pointer, camera );
+                    const intersects = this.raycaster.intersectObjects(Object.values(this.targets));
+                    for ( let i = 0; i < intersects.length; i ++ ) {
+                        if (this.modelReady
+                            && this.activeAction.getClip().name == "Unboxing"
+                            && this.mouseDown
+                            && intersects[i].object.name === "packaging")
+                        {
+                            if (this.x0 == undefined) {
+                                this.x0 = event.clientX
+                            }
+                            const completion =
+                                ((event.clientX - this.x0) *
+                                this.activeAction.getClip().duration) /
+                                ((event.target as HTMLCanvasElement).clientWidth / 2)
+                            intersects[i].object.material.transparent = true
+                            intersects[i].object.material.opacity = completion / this.activeAction.getClip().duration
+                            this.mixer.setTime(completion)
+                            if (completion >= this.activeAction.getClip().duration - (5 * this.activeAction.getClip().duration) / 100) {
+                                intersects[i].object.visible = false
+                                this.setAction(this.animationActions[2])
+                                this.activeAction.setLoop(THREE.LoopOnce)
+                                this.activeAction.clampWhenFinished = true
+                                this.triggerFinalAnimation(camera)
+                                this.blockLoop = true
+                            }
+                        }
+                    }
+                }
+            }
+        )
+    }
+
+    triggerFinalAnimation(camera) {
+        document.querySelector<HTMLCanvasElement>('#webgl')?.addEventListener(
+            'mousedown',
+            () => {
+                this.raycaster.setFromCamera( this.pointer, camera );
+                const intersects = this.raycaster.intersectObjects(Object.values(this.targets));
+                for ( let i = 0; i < intersects.length; i ++ ) {
+                    if(intersects[i].object.name === "packaging") {
+                        this.runLastAnim = true
+                        const targetCoords = {
+                            x: 0,
+                            y: 0.8343677459755188,
+                            z: 0.49586116341112374
+                        }
+                        const coords = { x: camera.position.x, y: camera.position.y, z: camera.position.z };
+                        new TWEEN.Tween(coords)
+                            .to({ x: targetCoords.x, y: targetCoords.y, z: targetCoords.z })
+                            .onUpdate(() =>
+                                camera.position.set(coords.x, coords.y, coords.z)
+                            )
+                            .start();
+                    }
+                }
+            }
+        )
+    }
+
+    anim(camera) {
+        TWEEN.update();
+        if(this.modelReady) {
+            this.raycaster.setFromCamera( this.pointer, camera );
+            if(this.blockLoop && this.runLastAnim) {
+                this.mixer.update(this.clock.getDelta())
+            }
+        }
+    }
+
+    setAction(toAction: THREE.AnimationAction) {
+        if (toAction != this.activeAction) {
+            this.lastAction = this.activeAction
+            this.activeAction = toAction
+            this.lastAction.stop()
+            //lastAction.fadeOut(1)
+            this.activeAction.reset()
+            //activeAction.fadeIn(1)
+            this.activeAction.play()
+        }
+    }
+
+    destroy() {
+
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,97 @@
 import './style.css'
+import * as THREE from "three"
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {Case} from "./Case";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const canvas = document.querySelector<HTMLDivElement>('canvas#webgl')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: canvas.clientWidth,
+    height: canvas.clientHeight,
+}
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setClearColor(0x000000, 1)
+renderer.setSize(sizes.width, sizes.height)
+
+/**
+* Camera
+*/
+const camera = new THREE.PerspectiveCamera(50, canvas.clientWidth / canvas.clientHeight, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+/**
+ * Objects
+ */
+const caseElement = new Case()
+caseElement.init(() => {
+    scene.add(caseElement.object)
+}, camera, controls)
+
+const light = new THREE.AmbientLight( 0x404040, 3.5 )
+// light.position.set(0.8, 1.4, 1.0)
+scene.add(light)
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    // const elapsedTime = clock.getElapsedTime()
+
+    // Check canvas size and resolution
+    if (resizeRendererToDisplaySize()) {
+        const aspect = canvas.clientWidth / canvas.clientHeight
+        if (camera.isPerspectiveCamera || camera.isOrthographicCamera || camera.isCamera) {
+            camera.aspect = aspect
+            camera.updateProjectionMatrix()
+        }
+    }
+
+    // Update controls
+    controls.update()
+
+    // if (modelReady) mixer.update(clock.getDelta())
+    caseElement.anim(camera)
+
+    // Render
+    render()
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+function resizeRendererToDisplaySize() {
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.clientWidth !== sizes.width || canvas.clientHeight !== sizes.height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+
+    return needResize
+}
+
+tick()
