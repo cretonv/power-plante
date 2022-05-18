cb3289691d0ad9db2699173dac79dee8f810760a
Vincent <vincecreton@hotmail.fr>
2022-05-18 09:03:56 +0200

    WIP: Starting static website

---
+++ src/website.ts
@@ -0,0 +1,8 @@
+import '../assets/css/main.css'
+import '../assets/css/locomotive-scroll.css'
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
