cc08e9e26fae5479c03a2bf9f71f9dc6658e66de
Vincent <vincecreton@hotmail.fr>
2022-06-03 23:02:52 +0200

    FEATURE: Add slider to static website

--- src/website.ts
+++ src/website.ts
@@ -1,6 +1,25 @@
 import '../assets/css/main.css'
 import '../assets/css/locomotive-scroll.css'
 import LocomotiveScroll from 'locomotive-scroll';
+import Swiper, { Navigation } from 'swiper';
+
+Swiper.use([Navigation]);
+
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    slidesPerView: 7,
+    spaceBetween: 32,
+    direction: 'horizontal',
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+        disabledClass: 'disabled'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
