580f655a07d493ba8a852f919f6ccf08c034a422
Vincent <vincecreton@hotmail.fr>
2022-06-06 10:16:19 +0200

    FIX: Close modal button  not work after merge

--- src/ModalViewport.ts
+++ src/ModalViewport.ts
@@ -128,7 +128,9 @@ export class ModalViewport {
         if(this.isVisible) {
             if (this.isFirstOpening) {
                 this.isFirstOpening = false
+                console.log(this.htmlDescriptionElement.querySelector('.close-cross'))
                 this.htmlDescriptionElement.querySelector('.close-cross').addEventListener('click', () => {
+                    console.log('CLICK')
                     this.closeModal()
                 })
             }
