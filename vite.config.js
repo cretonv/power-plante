3e407599a53ee08ef98fb9a6b2af8b7a2b7296a5
Vincent <vincecreton@hotmail.fr>
2022-06-08 15:33:29 +0200

    FIX : Pre-prod assets

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 // vite.config.js
 const {defineConfig} = require("vite");
+const { resolve } = require('path')
 export default defineConfig({
     build: {
         chunkSizeWarningLimit: 1000,
-        assetsInlineLimit: '2048' // 2kb
+        assetsInlineLimit: '2048', // 2kb
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                website: resolve(__dirname, 'website.html')
+            }
+        }
     },
     publicDir: 'public',
 })
